39a1256b7dbef9923ec289cbe31e3fea4e3c98b1
Anton Vrutskyi <antn.vryckij@gmail.com>
2025-11-11 09:47:35 +0200

    fix lint-staged

--- lint-staged.config.mjs
+++
@@ -1,9 +0,0 @@
-const config = {
-  '*.{js,ts,cjs,mjs,jsx,tsx}': [(files) => `eslint --fix --max-warnings=600 ${files.join(' ')}`],
-  '*.{css,scss}': [
-    (files) => `stylelint --fix ${files.join(' ')}`,
-    (files) => `prettier --write ${files.join(' ')}`,
-  ],
-};
-
-export default config;
