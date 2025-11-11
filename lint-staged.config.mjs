a549ddfecf073a7ba8470bface1c87443563ee97
Anton Vrutskyi <antn.vryckij@gmail.com>
2025-11-11 09:09:53 +0200

    fix lint-staged css rule

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,6 @@
 const config = {
-  '*.{js,ts,cjs,mjs,jsx,tsx}': [
-    (files) => `eslint --fix --max-warnings=600 ${files.join(' ')}`,
-  ],
-  '*.{css}': [
+  '*.{js,ts,cjs,mjs,jsx,tsx}': [(files) => `eslint --fix --max-warnings=600 ${files.join(' ')}`],
+  '*.{css,scss}': [
     (files) => `stylelint --fix ${files.join(' ')}`,
     (files) => `prettier --write ${files.join(' ')}`,
   ],
